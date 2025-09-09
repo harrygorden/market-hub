4b0212c3cdae3e0a8bedc9d2ed69526d5bfabd5a
harrygorden@gmail.com <harrygorden@gmail.com>
2025-09-09 03:03:16 +0000

    Edited Layout

--- client_code/Layout/__init__.py
+++ client_code/Layout/__init__.py
@@ -7,3 +7,19 @@ class Layout(LayoutTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_home_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('Home')
+
+  def button_news_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('News')
+
+  def button_events_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('Events')
+
+  def outlined_button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('Technicals')
